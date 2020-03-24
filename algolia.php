d719451d3a5e31cce9b1828d64fe850270931dd2
Antoine Girard <thetoine@gmail.com>
2020-03-24 12:45:24 -0400

    version bunmp

--- algolia.php
+++ algolia.php
@@ -5,7 +5,7 @@
  * GitHub Plugin URI:  https://github.com/Mill3/poleacn-algolia-sync-plugin
  * Plugin Name: Poleacn - Algolia Sync
  * Description: Sync data from Wordpress to Algolia
- * Version: 0.0.19
+ * Version: 0.0.20
  * Author Name: Mill3 Studio (Antoine Girard)
  *
  * @package CSTJ_Algolia_Sync
